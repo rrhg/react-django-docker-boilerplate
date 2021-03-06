8cd39c0e9943883508791201e14c86bcea6a5dbd
rrhg <rrh@fastmail.com>
2021-03-06 08:33:42 -0600

    Add About react frontend component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import {Context} from '../Context'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Card from 'react-bootstrap/Card';
-import {Link} from "react-router-dom"
 
 
 const About = props => {
